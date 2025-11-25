0acdfc09996ab1b49cff21e0eab60fe1cfe28690
abduxakimovotabek295-code <abduxakimovotabek295@gmail.com>
2025-11-25 11:14:11 +0500

    savatga qo'shish tayyor

--- components/Cards/Card_List.jsx
+++ components/Cards/Card_List.jsx
@@ -28,9 +28,31 @@ const Card_List = ({ data }) => {
                   {item.price} so'm
                 </Text>
 
-                <Group mt={"md"} grow gap={"xs"}>
+                <Group mt={"md"} grow gap="xs" >
                   <Button color="blue" size="compact-xs">
-                    Add
+                    <span
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "6px",
+                      }}
+                    >
+                      Savatga Qo'shish
+                      <svg
+                        width="14"
+                        height="14"
+                        viewBox="0 0 24 24"
+                        fill="none"
+                        stroke="currentColor"
+                        strokeWidth="2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <circle cx="9" cy="21" r="1"></circle>
+                        <circle cx="20" cy="21" r="1"></circle>
+                        <path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                      </svg>
+                    </span>
                   </Button>
                 </Group>
               </Card>
